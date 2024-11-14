7674d1ee0b864d3b9cf27ab6aa9fd14ae89dd852
Tessy Grondin <tessy.grondin@epitech.eu>
2024-11-14 09:58:13 +0400

    fixed size board

--- game.js
+++ game.js
@@ -52,8 +52,6 @@ let clickOnPhase = [selectClick, recipeClick, ingredientClick];
 
 
 
-let x4 = [{pos:7, win:false, value:0}, {pos:8, win:false, value:0}, {pos:9, win:false, value:0}, {pos:10, win:false, value:0}, {pos:13, win:false, value:0}, {pos:14, win:false, value:0}, {pos:15, win:false, value:0}, {pos:16, win:false, value:0}, {pos:19, win:false, value:0}, {pos:20, win:false, value:0}, {pos:21, win:false, value:0}, {pos:22, win:false, value:0}, {pos:25, win:false, value:0}, {pos:26, win:false, value:0}, {pos:27, win:false, value:0}, {pos:28, win:false, value:0}];
-let x3 = [{pos:8, win:false, value:0}, {pos:9, win:false, value:0}, {pos:14, win:false, value:0}, {pos:15, win:false, value:0}, {pos:20, win:false, value:0}, {pos:21, win:false, value:0}, {pos:26, win:false, value:0}, {pos:27, win:false, value:0}];
 let board = [];
 
 let end = false;
@@ -77,7 +75,7 @@ let y = (canvas.height - 32 * 6) / 2;
 
 for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 6; j++) {
-        board.push({x:x, y:y, value:0, activ:true, clicked:false, win:false});
+        board.push({x:x, y:y, value:0, clicked:false, win:false});
         x += 32;
     }
     x = (canvas.width - 32 * 6) / 2;
@@ -86,7 +84,7 @@ for (let i = 0; i < 6; i++) {
 
 context.fillStyle = 'yellow';
 
-function chosePattern(length) {
+function chosePattern() {
     let p1 = Math.floor(Math.random() * nbPattern);
     let p2 = Math.floor(Math.random() * nbPattern);
     let p3 = Math.floor(Math.random() * nbPattern);
@@ -103,171 +101,87 @@ function chosePattern(length) {
         p3 = Math.floor(Math.random() * nbPattern);
     while (p4 == p1 || p4 == p2 || p4 == p3)
         p4 = Math.floor(Math.random() * nbPattern);
-    if (length < x4.length) {
-        x3.forEach(function(ref) {
-            textChose = 0;
+
+    let nb1 = 15;
+    if (div == 0) {
+        if (textChose == 0)
             patternToShow[0] = p1;
+        else {
+            patternToShow[0] = p2;
+            patternToShow[1] = p3;
+        }
+        board.forEach(function(ref) {
             if (ref.win)
                 ref.value = p1;
-            else
-                ref.value = p2;
-        });
-    } else if (length < board.length) {
-        if (div == 0) {
-            if (textChose == 0)
-                patternToShow[0] = p1;
-            else
-                patternToShow[0] = p2;
-            x4.forEach(function(ref) {
-                if (ref.win)
-                    ref.value = p1;
-                else
+            else {
+                let choice = Math.floor(Math.random() * 2);
+                if (choice == 0 && nb1 > 0) {
                     ref.value = p2;
-            });
-        } else {
-            let nb = 7;
-            if (textChose == 0)
-                patternToShow[0] = p1;
-            else {
-                patternToShow[0] = p2;
-                patternToShow[1] = p3;
+                    nb1--;
+                } else
+                    ref.value = p3;
             }
-            x4.forEach(function(ref) {
-                if (ref.win)
-                    ref.value = p1;
-                else {
-                    let choice = Math.floor(Math.random() * 2);
-                    if (choice == 0 && nb > 0) {
-                        ref.value = p2;
-                        nb--;
-                    } else
-                        ref.value = p3;
-                }
-            });
-        }
+        });
     } else {
-        let nb1 = 15;
-        if (div == 0) {
-            if (textChose == 0)
-                patternToShow[0] = p1;
-            else {
-                patternToShow[0] = p2;
-                patternToShow[1] = p3;
-            }
-            board.forEach(function(ref) {
-                if (ref.win)
-                    ref.value = p1;
-                else {
+        let nb2 = 2;
+        if (textChose == 0) {
+            patternToShow[0] = p1;
+            patternToShow[1] = p4;
+        } else {
+            patternToShow[0] = p2;
+            patternToShow[1] = p3;
+        }
+        board.forEach(function(ref) {
+            if (ref.win) {
+                    let choice = Math.floor(Math.random() * 2);
+                    if (choice == 0 && nb2 > 0) {
+                        ref.value = p1;
+                        nb2--;
+                    } else
+                        ref.value = p4;
+            } else {
                     let choice = Math.floor(Math.random() * 2);
                     if (choice == 0 && nb1 > 0) {
                         ref.value = p2;
                         nb1--;
                     } else
                         ref.value = p3;
-                }
-            });
-        } else {
-            let nb2 = 2;
-            if (textChose == 0) {
-                patternToShow[0] = p1;
-                patternToShow[1] = p4;
-            } else {
-                patternToShow[0] = p2;
-                patternToShow[1] = p3;
             }
-            board.forEach(function(ref) {
-                if (ref.win) {
-                        let choice = Math.floor(Math.random() * 2);
-                        if (choice == 0 && nb2 > 0) {
-                            ref.value = p1;
-                            nb2--;
-                        } else
-                            ref.value = p4;
-                } else {
-                        let choice = Math.floor(Math.random() * 2);
-                        if (choice == 0 && nb1 > 0) {
-                            ref.value = p2;
-                            nb1--;
-                        } else
-                            ref.value = p3;
-                }
-            });
-        }
+        });
     }
 }
 
-function choseWin(length) {
-    let w1 = Math.floor(Math.random() * length);
-    let w2 = Math.floor(Math.random() * length);
-    let w3 = Math.floor(Math.random() * length);
-    let w4 = Math.floor(Math.random() * length);
-    let w5 = Math.floor(Math.random() * length);
+function choseWin() {
+    let w1 = Math.floor(Math.random() * board.length);
+    let w2 = Math.floor(Math.random() * board.length);
+    let w3 = Math.floor(Math.random() * board.length);
+    let w4 = Math.floor(Math.random() * board.length);
+    let w5 = Math.floor(Math.random() * board.length);
 
     while (w2 == w1)
-        w2 = Math.floor(Math.random() * length);
+        w2 = Math.floor(Math.random() * board.length);
     while (w3 == w1 || w3 == w2)
-        w3 = Math.floor(Math.random() * length);
+        w3 = Math.floor(Math.random() * board.length);
     while (w4 == w1 || w4 == w2 || w4 == w3)
-        w4 = Math.floor(Math.random() * length);
+        w4 = Math.floor(Math.random() * board.length);
     while (w5 == w1 || w5 == w2 || w5 == w3 || w5 == w4)
-        w5 = Math.floor(Math.random() * length);
-    if (length < x4.length) {
-        x3[w1].win = true;
-        x3[w2].win = true;
-    } else if (length < board.length) {
-        x4[w1].win = true;
-        x4[w2].win = true;
-        x4[w3].win = true;
-        x4[w4].win = true;
-    } else {
-        board[w1].win = true;
-        board[w2].win = true;
-        board[w3].win = true;
-        board[w4].win = true;
-        board[w5].win = true;
-    }
+        w5 = Math.floor(Math.random() * board.length);
+    board[w1].win = true;
+    board[w2].win = true;
+    board[w3].win = true;
+    board[w4].win = true;
+    board[w5].win = true;
 }
 
 function makeBoard() {
-    let type = Math.floor(Math.random() * 3);
 
-    x3.forEach(function(n) {
-        n.win = false;
-        n.value = 0;
-    });
-    x4.forEach(function(n) {
-        n.win = false;
-        n.value = 0;
-    });
-    board.forEach(function(tile){
-        tile.activ = false;
+    board.forEach(function(tile) {
         tile.value = 0;
         tile.clicked = false;
         tile.win = false;
     });
-    if (type == 1) {
-        choseWin(x4.length);
-        chosePattern(x4.length);
-        x4.forEach(function(n) {
-            board[n.pos].activ = true;
-            board[n.pos].win = n.win;
-            board[n.pos].value = n.value;
-        });
-    } else if (type == 0) {
-        choseWin(x3.length);
-        chosePattern(x3.length);
-        x3.forEach(function(n) {
-            board[n.pos].activ = true;
-            board[n.pos].win = n.win;
-            board[n.pos].value = n.value;
-        });
-    } else {
-        choseWin(board.length);
-        chosePattern(board.length);
-        board.forEach(function(tile) {
-            tile.activ = true;
-        });
-    }
+    choseWin();
+    chosePattern();
 }
 
 
@@ -332,14 +246,12 @@ function ingredientPhase() {
     board.forEach(function(tile) {
         if ((tile.clicked && tile.win) || !tile.win)
             i++;
-        if (tile.activ) {
-            context.drawImage(assetsImage, 640, 0, 64, 64, tile.x, tile.y, 32, 32);
-            context.drawImage(assetsImage, tile.value * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
-            if (tile.clicked && tile.win)
-                context.drawImage(assetsImage, 11 * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
-            if (tile.clicked && !tile.win)
-                context.drawImage(assetsImage, 12 * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
-        }
+        context.drawImage(assetsImage, 640, 0, 64, 64, tile.x, tile.y, 32, 32);
+        context.drawImage(assetsImage, tile.value * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
+        if (tile.clicked && tile.win)
+            context.drawImage(assetsImage, 11 * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
+        if (tile.clicked && !tile.win)
+            context.drawImage(assetsImage, 12 * 64, 0, 64, 64, tile.x, tile.y, 32, 32);
     });
     if (i == board.length)
         phase++;
@@ -399,7 +311,7 @@ function recipeClick(relativeX, relativeY) {
 
 function ingredientClick(relativeX, relativeY) {
     board.forEach(function(boardtile) {
-        if (!boardtile.activ || relativeX < boardtile.x || relativeX > boardtile.x + 32 || relativeY < boardtile.y || relativeY > boardtile.y + 32)
+        if (relativeX < boardtile.x || relativeX > boardtile.x + 32 || relativeY < boardtile.y || relativeY > boardtile.y + 32)
             return;
         if (!boardtile.clicked && boardtile.win)
             score += 100;
